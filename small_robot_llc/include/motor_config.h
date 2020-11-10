a88b7a2580ba623ce98ac1d96ed1cd808530a332
Maxie Chang <maxie.x@valsch.com>
2020-11-10 22:08:24 +0800

    Fix previous commit issues

--- small_robot_llc/include/motor_config.h
+++ small_robot_llc/include/motor_config.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #ifndef MOTOR_CONFIG_H
 #define MOTOR_CONFIG_H
 
@@ -15,22 +15,4 @@
 #define MOTOR_DUTY_MIN_ABS    (0.15f)
 
 
-=======
-#ifndef MOTOR_CONFIG_H
-#define MOTOR_CONFIG_H
-
-
-/*Predefine macros*/
-#define MOTOR_ERR_THD         2
-#define MOTOR_SPEED_MAX_ABS   8000
-#define MOTOR_SPEED_MIN_ABS   100
-#define MOTOR_PULSE_PER_METER 21738
-#define TRACK_WIDTH_M         0.125
-
-#define MOTOR_DDUTY_MAX001    (20.0f)     
-#define MOTOR_DUTY_MAX_ABS    (1.0f)
-#define MOTOR_DUTY_MIN_ABS    (0.15f)
-
-
->>>>>>> a1aecd2... Initial commit
 #endif
